0378e245b05ef27b2fd508decf0dcd6c811d59bb
Christoh Prenissl <christophprenissl@gmail.com>
2025-04-13 16:24:58 +0200

    Initial Commit

---
+++ WatchList/WatchListApp.swift
@@ -0,0 +1,32 @@
+//
+//  WatchListApp.swift
+//  WatchList
+//
+//  Created by Christoph Prenissl on 13.04.25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WatchListApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ WatchListTests/WatchListTests.swift
@@ -0,0 +1,17 @@
+//
+//  WatchListTests.swift
+//  WatchListTests
+//
+//  Created by Christoph Prenissl on 13.04.25.
+//
+
+import Testing
+@testable import WatchList
+
+struct WatchListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
