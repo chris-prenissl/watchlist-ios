39e9ed519425c764b529f3e223e988265ee6af8d
Christoh Prenissl <christophprenissl@gmail.com>
2025-06-09 21:00:22 +0200

    Remove unnecessary property and do catch block

--- WatchList/search/presentation/SearchViewModel.swift
+++ WatchList/search/presentation/SearchViewModel.swift
@@ -1,24 +1,19 @@
-import Foundation
 import Combine
+import Foundation
 
 class SearchViewModel: ObservableObject {
     private let searchRepository: SearchRepository
-    
+
     @Published var movieSearchItems: [MovieSearchItem] = []
-    private var cancellables: Set<AnyCancellable> = []
-    
+
     init(searchRepository: SearchRepository) {
         self.searchRepository = searchRepository
     }
-    
+
     func searchMovies(query: String) async {
-        do {
-            let result = try await searchRepository.search(query: query)
-            await MainActor.run {
-                movieSearchItems = result
-            }
-        } catch {
-            print("Error: \(error)")
+        let result = await searchRepository.search(query: query)
+        await MainActor.run {
+            movieSearchItems = result
         }
     }
 }
